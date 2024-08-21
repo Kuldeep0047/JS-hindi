22ca527039a4d0fa79cfef467cc0dc130f18926d
Kuldeep Tiwari <tiwarikuldeep307@gmail.com>
2024-08-21 19:46:33 +0530

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all Js code as newer version
+console.log("my name is kuldeep")
+let name = "kuldeep"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bpolean => true/false
+//null => standalone value
+// undefined => variable is stored but no value is given or vo khali hai
+// symbol => unique
+
+
+
+
+// object 
+
+console.log(typeof "age");
+
